06dc5922d73b063437b8047cc6c4963ef60baef1
Nemenwq <chloemikka@gmail.com>
2026-04-29 08:15:41 +0100

    fix: migeration error

--- api/src/db/data-source.ts
+++ api/src/db/data-source.ts
@@ -24,6 +24,8 @@ import { PlatformConfig } from "../entities/PlatformConfig";
 import { Report } from "../entities/Report";
 import { WebhookSubscription } from "../entities/WebhookSubscription";
 import { WebhookDeliveryLog } from "../entities/WebhookDeliveryLog";
+import { Role } from "../entities/Role";
+import { UserRole } from "../entities/UserRole";
 
 export const buildDataSource = (databaseUrl = env.databaseUrl) =>
   new DataSource({
@@ -31,7 +33,7 @@ export const buildDataSource = (databaseUrl = env.databaseUrl) =>
     ...(databaseUrl.startsWith("sqljs")
       ? { location: databaseUrl.replace("sqljs://", ""), autoSave: false }
       : { url: databaseUrl }),
-    entities: [Grant, Milestone, MilestoneProof, User, GrantReviewer, MilestoneApproval, Contributor, ReputationLog, AuditLog, UserWatchlist, Activity, GrantView, ReconciliationCheckpoint, RateLimitLog, Community, MilestoneComment, GrantHistory, FeeCollection, PlatformConfig, Report, WebhookSubscription, WebhookDeliveryLog],
+    entities: [Grant, Milestone, MilestoneProof, User, GrantReviewer, MilestoneApproval, Contributor, ReputationLog, AuditLog, UserWatchlist, Activity, GrantView, ReconciliationCheckpoint, RateLimitLog, Community, MilestoneComment, GrantHistory, FeeCollection, PlatformConfig, Report, WebhookSubscription, WebhookDeliveryLog, Role, UserRole],
     synchronize: true,
   });
 
